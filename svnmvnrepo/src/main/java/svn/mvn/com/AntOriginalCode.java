ed06dc9035e7d70f897f472bfcdb654b62403203
sumanthgithub <sumanthacademic@gmail.com>
2017-04-14 19:13:54 -0700

    svnmvnrepo added

---
+++ svnmvnrepo/src/main/java/svn/mvn/com/AntOriginalCode.java
@@ -0,0 +1,7 @@
+class AntOriginalCode
+{ 
+  public static void main (String [] args)
+  {
+    System.out.println("Welcome to the Program which is used to create a successful build using Java Code, Ant and Jenkins");
+   }
+}
---
+++ svnmvnrepo/src/main/java/svn/mvn/com/App.java
@@ -0,0 +1,13 @@
+package svn.mvn.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
